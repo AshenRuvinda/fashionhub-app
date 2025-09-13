6b6e13374564a36276c8bea9e42342cfba7a6711
Ashen Ruvinda <138576609+AshenRuvinda@users.noreply.github.com>
2025-09-13 17:50:49 +0530

    init

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import Onboarding from '../pages/Onboarding';
+import Details from '../pages/Details';
+import Cart from '../pages/Cart';
+import Checkout from '../pages/Checkout';
+
+export default function AppRoutes() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/onboarding" element={<Onboarding />} />
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+    </Router>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./public/index.html",
+      "./src/**/*.{js,jsx}", // Scan all JSX files
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
